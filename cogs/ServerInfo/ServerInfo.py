f90902cd309cedb37c20f1f27d21a281340918fc
xyz-q <matthewrachao@gmail.com>
2024-12-12 11:38:16 -0700

    removed dm help

--- cogs/ServerInfo/ServerInfo.py
+++ cogs/ServerInfo/ServerInfo.py
@@ -89,25 +89,7 @@ class ServerInfo(commands.Cog):
         else:
             await ctx.send(f"An error occurred: {str(error)}")
 
-    @commands.command()
-    async def serverhelp(ctx):
-        help_text = """
-    **Available Commands:**
-    ,voiceinfo <server_id> - Get information about voice channels in a server
-    ,servermembers <server_id> - Get a list of members in a server
-    ,serverlist
-    ,channellist <server_id> - Get a list of channels in a server
-    ,serverjoin <server_id> [channel_id] - Join a server's vc (optional channel ID)
-    ,inviteserver <server_id> - Get an invite link for a server
-    ,messages <serverid> <channelid> <limit> - Get messages from a channel
 
-
-
-
-
-
-        """
-        await ctx.send(help_text)
 
 
 async def setup(bot):
