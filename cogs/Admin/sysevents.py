3cbed734082dfe93b33a73fb8cd8e00cf2561f04
xyz-q <matthewrachao@gmail.com>
2025-01-06 12:46:38 -0700

    Update sysevents.py

--- cogs/Admin/sysevents.py
+++ cogs/Admin/sysevents.py
@@ -123,7 +123,7 @@ class SystemEvents(commands.Cog):
                     return
 
                 # Role check
-                required_role = discord.utils.get(message.guild.roles, name='Bot Commander')
+                required_role = discord.utils.get(message.guild.roles, name='admin-commands')
                 if required_role not in message.author.roles:
                     try:
                         warning = await message.channel.send("❌ You don't have permission to use this command!")
