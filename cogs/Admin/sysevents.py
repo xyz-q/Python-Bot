ae7bff93971b2c83149ad2c0e1aa0a3593b412a4
xyz-q <matthewrachao@gmail.com>
2025-01-25 12:54:40 -0700

    Update sysevents.py

--- cogs/Admin/sysevents.py
+++ cogs/Admin/sysevents.py
@@ -106,17 +106,15 @@ class SystemEvents(commands.Cog):
             message.content = message.content.lower()
             allowed_commands = (',pc', ',help', ',invite', ',slots', ',flower', ',bal', ',balance', ',staking', ',deposit', ',withdraw', ',stats', ',transfer', ',send')
 
-            if message.content.startswith(allowed_commands):
-                print(f"\033[0;32mCommand: {message.content} by {message.author}\033[0m")
-                await self.bot.process_commands(message)
-                return
+
 
             if message.channel.name != 'admin-commands':
                 if message.author.id == 110927272210354176:
-                    await self.bot.process_commands(message)
-                    print(f"\033[0;32mCommand: {message.content} by {message.author}\033[0m")
-                    print("Processing command from owner..")
-                    return
+                    if message.content.startswith(allowed_commands):
+                        await self.bot.process_commands(message)
+                        print(f"\033[0;32mCommand: {message.content} by {message.author}\033[0m")
+                        print(f"\033[0;32mBypassing command '{message.content}' from {message.author} in channel #{message.channel.name}\033[0m")
+                        return
 
                 try:
                     warning = await message.channel.send("❌ Please use commands in #admin-commands")
