490d5e193e6ef9dffdd982c1daedca740cb45ae3
xyz-q <matthewrachao@gmail.com>
2026-08-02 00:27:54 -0600

    Update maintenance.py

--- cogs/Admin/maintenance.py
+++ cogs/Admin/maintenance.py
@@ -164,14 +164,6 @@ class Maintenance(commands.Cog):
             except discord.Forbidden as e:
                 print(f"Missing permissions to update nickname in {guild.name}: {e}")
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, MaintenanceMode):
-            await ctx.send(
-                embed=self.create_blocked_embed(),
-                delete_after=5
-            )
-            return
 
     @commands.Cog.listener()
     async def on_ready(self):
